45d5547f46376e49462a19bd92f29eb2869032db
Xiaojie Zhu <xzfor3@mac.lan>
2025-08-31 08:51:42 -0500

    check in

--- Problem37.cpp
+++ Problem37.cpp
@@ -4,39 +4,34 @@ using std::vector;
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        helper1(board, 0, 0);
+        dfs(board, 0, 0);
     }
 private:
-    bool helper1(vector<vector<char>>& board , int row , int col){
-        if(row == 9) return true ;
-        if(col == 9) return helper1(board , row+1 , 0) ;
-        if(board[row][col] != '.') return helper1(board , row , col + 1) ;
-
-        for(int i=1 ; i<=9 ; i++){
-            if(helper2(board , row , col , i)){
-                board[row][col] = '0' + i ;
-                bool res = helper1(board , row , col+1) ;
-                if(res) return true ;
-                board[row][col] = '.' ;
-            }
+    bool dfs(vector<vector<char>>& board, int i , int j) {
+        if (i == 9) return true;
+        if (j == 9) return dfs(board, i + 1 , 0);
+        if (board[i][j] != '.') return dfs(board, i, j + 1);
+        for (char c = '1'; c <= '9'; c++) {
+            if (!yes(board, i, j, c)) continue;
+            board[i][j] = c;
+            if (dfs(board, i, j + 1)) return true;
+            board[i][j] = '.';
         }
-        return false ;
+        return false;
     }
-    bool helper2(vector<vector<char>>& board , int row , int col , int num){
-        for(int i=0 ; i<9 ; i++){
-            if(board[row][i]== '0'+num) return false ;
+    bool yes(vector<vector<char>>& board, int i, int j, int c) {
+        for (int m = 0; m < 9; m++) {
+            if (board[i][m] == c) return false;
         }
-        for(int j=0 ; j<9 ; j++){
-            if(board[j][col] == '0'+num) return false ;
+        for (int m = 0; m < 9; m++) {
+            if (board[m][j] == c) return false;
         }
-        int x = (row/3)*3 ;
-        int y = (col/3)*3 ;
-
-        for(int i=x ; i<x+3 ; i++){
-            for(int j=y ; j<y+3 ; j++){
-                if(board[i][j]=='0'+num) return false ;
+        int m = (i / 3) * 3, n = (j / 3) * 3;
+        for (int k = 0; k < 3; k++) {
+            for (int t = 0; t < 3; t++) {
+                if (board[m + k][n + t] == c) return false;
             }
         }
-        return true ;
+        return true;
     }
 };
