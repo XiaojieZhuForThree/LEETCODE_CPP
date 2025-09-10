10cb0a2ded2b7d772f64573363c1bec49776440a
Xiaojie Zhu <xzfor3@Mac.lan>
2025-09-09 20:53:15 -0500

    check in

--- Problem1733.cpp
+++ Problem1733.cpp
@@ -1,37 +1,38 @@
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 using std::vector;
 
 class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
-        int m = size(languages);
-        vector<vector<char>> matrix(m + 1, vector<char>(n + 1, false));
-        for (int i = 0; i < m; i++) {
-            for (auto& language : languages[i]) matrix[i + 1][language] = true;
-        }
-        vector<char> learn(m + 1, false);
-        for (auto& friendship : friendships) {
-            bool can = false;
-            for (int i = 1; i <= n; i++) {
-                if (matrix[friendship[0]][i] && matrix[friendship[1]][i]) {
-                    can = true;
-                    break;
-                }
-            }
-            if (!can) {
-                learn[friendship[0]] = true;
-                learn[friendship[1]] = true;
-            }
+        vector<std::unordered_set<int>> ls(size(languages));
+        for (int i = 0; i < size(languages); i++) {
+            for (int j : languages[i]) ls[i].insert(j);
         }
         int ans = INT_MAX;
         for (int i = 1; i <= n; i++) {
             int cur = 0;
-            for (int j = 1; j <= m; j++) {
-                if (learn[j] && !matrix[j][i]) cur++;
+            std::unordered_set<int> checked;
+            for (auto& f : friendships) {
+                if (yes(f, ls)) continue;
+                cur += add(f[0] - 1, i, ls, checked);
+                cur += add(f[1] - 1, i, ls, checked);
             }
             ans = std::min(ans, cur);
         }
         return ans;
+    }
+private:
+    bool yes(vector<int>& f, vector<std::unordered_set<int>>& ls) {
+        for (int i : ls[f[0] - 1]) {
+            if (ls[f[1] - 1].count(i)) return true;
+        }
+        return false;
+    }
+    int add(int j, int i, vector<std::unordered_set<int>>& ls, std::unordered_set<int>& checked) {
+        if (checked.count(j)) return 0;
+        checked.insert(j);
+        if (ls[j].count(i)) return 0;
+        return 1;
     }
 };
