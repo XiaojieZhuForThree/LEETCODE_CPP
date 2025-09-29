cbcc0e07d55dc5c1c9b32ee8bd72b57269ef4ec3
Xiaojie Zhu <xzfor3@Mac.lan>
2025-09-28 19:30:58 -0500

    check in

--- Problem1039.cpp
+++ Problem1039.cpp
@@ -11,12 +11,10 @@ private:
     int helper(vector<int> &v){
         int n = v.size();
         vector<vector<int> > dp(n, vector<int> (n, 0));
-        for(int i = n - 1; i >= 0; i--){
-            for(int j= i + 2 ; j < n; j++){
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j= i + 2 ; j < n; j++) {
                 int ans = INT_MAX;
-                for(int k = i + 1; k < j; k++){
-                    ans = std::min(ans, v[i] * v[j] * v[k] + dp[i][k] + dp[k][j]);
-                }
+                for (int k = i + 1; k < j; k++) ans = std::min(ans, v[i] * v[j] * v[k] + dp[i][k] + dp[k][j]);
                 dp[i][j] = ans;
             }
         }
