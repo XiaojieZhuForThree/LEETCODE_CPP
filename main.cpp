5cb2d9e999f9031e5d2eee20aab27f7547145800
Xiaojie Zhu <xzfor3@Mac.lan>
2025-10-20 20:27:16 -0500

    check in

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
+    long long ans = 0;
+    for (int i = 1; i <= 10000; i++) ans += i * i;
+    std::cout << ans << std::endl;
     return 1;
 }
