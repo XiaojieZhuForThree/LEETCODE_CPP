b5b3c95b6dc47439468de415583f6f29111c4b9b
Xiaojie Zhu <xzfor3@Xiaojies-MacBook-Pro.local>
2025-11-15 19:22:49 -0500

    Add

--- Problem1513.cpp
+++ Problem1513.cpp
@@ -1,11 +1,12 @@
 #include <string>
+#include <vector>
 using std::string;
 class Solution {
 public:
     int numSub(string s) {
         int n = size(s);
         int ans = 0;
-        vector<int> dp(n + 1, 0);
+        std::vector<int> dp(n + 1, 0);
         for (int i = 1; i <= n; i++) {
             if (s[i - 1] == '0') dp[i] = 0;
             else {
