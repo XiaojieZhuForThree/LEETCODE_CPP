68fdfa1cd525144818731be57db58b265d59ed64
Xiaojie Zhu <xzfor3@Xiaojies-MacBook-Pro.local>
2025-11-29 00:21:39 -0800

    Add

---
+++ Problem3512.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int ans = 0;
+        for (int i : nums) ans += i;
+        return ans % k;
+    }
+};
