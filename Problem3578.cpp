67e0d023f61b148fa7aedbdcb5708f5bdb8799fb
Xiaojie Zhu <xzfor3@Mac.lan>
2025-12-05 19:07:57 -0600

    Add

---
+++ Problem3578.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <deque>
+using std::vector;
+
+class Solution {
+public:
+    int countPartitions(vector<int>& nums, int k) {
+        int n = size(nums), mod = 1e9 + 7, acc = 1;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+
+        std::deque<int> minq, maxq;
+        for (int i = 0, j = 0; j < n; j++) {
+            while (!maxq.empty() && nums[j] > nums[maxq.back()]) maxq.pop_back();
+            maxq.push_back(j);
+            while (!minq.empty() && nums[j] < nums[minq.back()]) minq.pop_back();
+            minq.push_back(j);
+            while (nums[maxq.front()] - nums[minq.front()] > k) {
+                acc = (acc - dp[i++] + mod) % mod;
+                if (minq.front() < i) minq.pop_front();
+                if (maxq.front() < i) maxq.pop_front();
+            }
+            dp[j + 1] = acc;
+            acc = (acc + dp[j + 1]) % mod;
+        }
+        return dp[n];        
+    }
+};
