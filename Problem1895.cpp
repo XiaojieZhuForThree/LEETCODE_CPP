6081942db3c0eb909fc5e524f734f5e79e0562cb
Xiaojie Zhu <xzfor3@Mac.lan>
2026-01-17 19:06:49 -0600

    Add

--- Problem1895.cpp
+++ Problem1895.cpp
@@ -8,8 +8,8 @@ public:
     int largestMagicSquare(vector<vector<int>> &grid) {
         m = grid.size(); n = grid[0].size();
         preSumRow = vector(m, vector<int>(n + 1, 0)); preSumCol = vector(n, vector<int>(m + 1, 0));
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
                 preSumRow[r][c + 1] = preSumRow[r][c] + grid[r][c];
                 preSumCol[c][r + 1] = preSumCol[c][r] + grid[r][c];
             }
@@ -18,16 +18,16 @@ public:
         return 1;
     }
     bool test(vector<vector<int>> &grid, int k) {
-        for (int r = 0; r + k - 1 < m; ++r) {
-            for (int c = 0; c + k - 1 < n; ++c) {
+        for (int r = 0; r + k - 1 < m; r++) {
+            for (int c = 0; c + k - 1 < n; c++) {
                 int diag = 0, antiDiag = 0;
-                for (int d = 0; d < k; ++d) {
+                for (int d = 0; d < k; d++) {
                     diag += grid[r + d][c + d];
                     antiDiag += grid[r + d][c + k - 1 - d];
                 }
                 bool match = diag == antiDiag;
-                for (int nr = r; nr < r + k && match; ++nr) match = diag == getSumRow(nr, c, c + k - 1);
-                for (int nc = c; nc < c + k && match; ++nc) match = diag == getSumCol(nc, r, r + k - 1);
+                for (int nr = r; nr < r + k && match; nr++) match = diag == getSumRow(nr, c, c + k - 1);
+                for (int nc = c; nc < c + k && match; nc++) match = diag == getSumCol(nc, r, r + k - 1);
                 if (match) return true;
             }
         }
