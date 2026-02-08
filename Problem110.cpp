936431c3b98b46eb37da0a9a44eab6eb928b3dea
Xiaojie Zhu <xzfor3@Xiaojies-MacBook-Pro.local>
2026-02-08 07:01:58 -0600

    Add

---
+++ Problem110.cpp
@@ -0,0 +1,15 @@
+#include "TreeNode.h"
+using std::pair;
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return helper(root).first;
+    }
+private:
+    pair<bool, int> helper(TreeNode* node) {
+        if (node == nullptr) return {true, 0};
+        auto l = helper(node->left), r = helper(node->right);
+        return {l.first && r.first && std::abs(l.second - r.second) <= 1, std::max(l.second, r.second) + 1};
+    }
+};
