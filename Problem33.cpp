32d9a282bb6492094e6268a40a96b4fe0345a65d
Xiaojie Zhu <xiaojie.zhu@outlook.com>
2026-05-21 19:02:44 -0500

    Add a new problem/solution

---
+++ Problem33.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i < size(nums); i++) {
+            if (nums[i] == target) return i;
+        }
+        return -1;
+    }
+};
