013b82595eb6b4dcdd9b2501f3e7ebaf28efc8b8
Xiaojie Zhu <xiaojie.zhu@outlook.com>
2026-09-22 19:06:30 -0500

    Add a new problem/solution

--- Problem1658.cpp
+++ Problem1658.cpp
@@ -8,9 +8,7 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size() ;
         int sum = 0 ;
-        for (int i = 0 ; i < nums.size() ; i++){
-            sum += nums[i] ;
-        }
+        for (int i = 0 ; i < nums.size() ; i++) sum += nums[i];
         int target = sum - x ;
         if (target < 0) return -1 ;
         if (target == 0) return n ;
@@ -19,16 +17,14 @@ public:
         int right = 0 ;
         int curr = 0 ;
         int operation = INT_MAX ;
-        while(right < n){
+        while(right < n) {
             curr += nums[right] ;
             right++ ;
             while(curr > target && left < n){
                 curr -= nums[left] ;
                 left++ ;
             }
-            if (curr == target){
-                operation = min(operation , n-(right-left)) ;
-            }
+            if (curr == target) operation = min(operation , n-(right-left));
         }
         if (operation == INT_MAX) return -1 ;
         return operation ;
